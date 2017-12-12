99e5ed2dd0b09318551e3f48d6c6bf64667ce9ec
Marc Neumann <marc.neumann@bmw.de>
2017-12-12 23:37:37 +0100

    implemented image undistorter

---
+++ camera_img_undistorter.py
@@ -0,0 +1,51 @@
+import numpy as np
+import glob
+import cv2
+import logging
+
+class CameraImageUndistorter(object):
+    """
+        Supports undistorting camera images after beeing calibrated with given chessboard
+        images from the very same camera.
+    """
+
+    mtx = None
+    dist = None
+
+    def __init__(self):
+        nx = 9
+        ny = 6
+
+        obj_points = []
+        img_points = []
+        img_shape = None
+
+        obj_points_single = np.zeros((nx * ny, 3), np.float32)
+        obj_points_single[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)  # TODO explain
+
+        image_files = glob.glob('./camera_cal/calibration*.jpg')
+        for img_file in image_files:
+            img = cv2.imread(img_file)
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            if img_shape is None:
+                img_shape = gray.shape
+
+            ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+            if ret:
+                img_points.append(corners)
+                obj_points.append(obj_points_single)
+            else:
+                logging.warning("camera calibration could not use the following image - not all chessboard corners were found: " + img_file)
+
+        ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img_shape[::-1], None, None)
+
+    def undistort(self, img):
+        """
+        Undistorts the given image.
+        :param img: input image
+        :return: undistorted image
+        """
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
--- lane_find.py
+++ lane_find.py
@@ -2,29 +2,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from camera_img_undistorter import CameraImageUndistorter
 
 
 def chessboard():
-    nx = 9
-    ny = 5
-
-    # Make a list of calibration images
-    fname = 'camera_cal/calibration1.jpg'
-    img = cv2.imread(fname)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
-    # If found, draw corners
-    if ret:
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
-        plt.imshow(img)
-        plt.show()
-        print("ok")
-    else:
-        print("failed")
+    img_undistorter = CameraImageUndistorter()
+    img_test_undistorted = img_undistorter.undistort(cv2.imread('./camera_cal/calibration1.jpg'))
+    plt.imshow(img_test_undistorted)
+    plt.show()
 
 if __name__ == '__main__':
     chessboard()
