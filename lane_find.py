4307354b48bfd82e821e7261771b96aaa474df34
Marc Neumann <marc.neumann@bmw.de>
2017-12-21 22:38:47 +0100

    plot binary image after sobel works

--- lane_find.py
+++ lane_find.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from matplotlib.colors import ListedColormap
 from camera_img_undistorter import CameraImageUndistorter
 from camera_birdview_transform import CameraImagePerspectiveTransform
 
 # Define a function that takes an image, gradient orientation,
 # and threshold min / max values.
 def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # TODO only sobel x required?
     # Apply x or y gradient with the OpenCV Sobel() function
     # and take the absolute value
@@ -32,6 +33,9 @@ def plot_images(img_src, img_dst):
     ax2.imshow(img_dst, cmap='gray')
     ax2.set_title('Destination Image', fontsize=50)
     ax2.imshow(img_dst, cmap='gray') # TODO make a parameter
+    #ax2.imshow(img_dst, cmap='binary') # TODO make a parameter
+    #ax2.imshow(img_dst, cmap=ListedColormap(['w', 'k'])) # TODO make a parameter
+    # TODO ax2.imshow(img_dst, cmap=map, interpolation='nearest') # TODO make a parameter
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
 
@@ -45,10 +49,13 @@ def chessboard():
     #plot_images(img, img_unwarped)
 
     img_undistorted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_binary = abs_sobel_thresh(img_undistorted)
-    plot_images(img_undistorted, img_binary)
+    img_binary = abs_sobel_thresh(img_undistorted, thresh_min=20, thresh_max=100)
+    #plot_images(img_undistorted, img_binary)
 
+    # TODO use magnitude of gradient
+    # TODO use direction of gradient
     # TODO filter interesting colors
+
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     s_channel = hls[:, :, 2]
     # TODO filter area where lanes can be
@@ -59,6 +66,10 @@ def chessboard():
     # TODO create binary image of lane lines
     # TODO detect lane lines
     #img_sobel = abs_sobel_thresh(img)
+
+    histogram = np.sum(img_birdview[img_birdview.shape[0] // 2:, :], axis=0)
+    plt.plot(histogram)
+    plt.show()
 
 
 if __name__ == '__main__':
