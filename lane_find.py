ca37ae247557e698a7ce481c52708b73ea24d6c7
Marc Neumann <marc.neumann@bmw.de>
2017-12-22 19:55:49 +0100

    added line isolator using sobel

--- lane_find.py
+++ lane_find.py
@@ -5,24 +5,7 @@ import matplotlib.image as mpimg
 from matplotlib.colors import ListedColormap
 from camera_img_undistorter import CameraImageUndistorter
 from camera_birdview_transform import CameraImagePerspectiveTransform
-
-# Define a function that takes an image, gradient orientation,
-# and threshold min / max values.
-def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # TODO only sobel x required?
-    # Apply x or y gradient with the OpenCV Sobel() function
-    # and take the absolute value
-    if orient == 'x':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
-    if orient == 'y':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
-    # Rescale back to 8 bit integer
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
-    binary_output = np.zeros_like(scaled_sobel) # TODO why create a copy?
-    binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
-
-    return binary_output
+from lane_isolator import LaneIsolator
 
 
 def plot_images(img_src, img_dst):
@@ -32,25 +15,27 @@ def plot_images(img_src, img_dst):
     ax1.set_title('Source Image', fontsize=50)
     ax2.imshow(img_dst, cmap='gray')
     ax2.set_title('Destination Image', fontsize=50)
-    ax2.imshow(img_dst, cmap='gray') # TODO make a parameter
-    #ax2.imshow(img_dst, cmap='binary') # TODO make a parameter
-    #ax2.imshow(img_dst, cmap=ListedColormap(['w', 'k'])) # TODO make a parameter
-    # TODO ax2.imshow(img_dst, cmap=map, interpolation='nearest') # TODO make a parameter
+    ax2.imshow(img_dst, cmap='gray')
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-    plt.show()
+    plt.show(block=True)
 
 
 def chessboard():
     img_transformer = CameraImagePerspectiveTransform()
     img_undistorter = CameraImageUndistorter()
+    lane_isolator = LaneIsolator()
 
     img = cv2.imread('./test_images/test1.jpg')
     img_undistorted = img_undistorter.undistort(img)
     #plot_images(img, img_unwarped)
 
     img_undistorted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_binary = abs_sobel_thresh(img_undistorted, thresh_min=20, thresh_max=100)
-    #plot_images(img_undistorted, img_binary)
+
+    # Choose a Sobel kernel size
+    ksize = 3  # Choose a larger odd number to smooth gradient measurements
+
+    img_binary = img_undistorted # TODO abs_sobel_thresh(img_undistorted, thresh_min=20, thresh_max=100)
+    plot_images(img_undistorted, img_binary)
 
     # TODO use magnitude of gradient
     # TODO use direction of gradient
@@ -68,8 +53,8 @@ def chessboard():
     #img_sobel = abs_sobel_thresh(img)
 
     histogram = np.sum(img_birdview[img_birdview.shape[0] // 2:, :], axis=0)
-    plt.plot(histogram)
-    plt.show()
+    #plt.plot(histogram)
+    plt.show(block=True)
 
 
 if __name__ == '__main__':
---
+++ lane_isolator.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+
+class LaneIsolator(object):
+
+    def __init__(self,
+                 ksize=3,
+                 gradx_thresh=(20, 100),
+                 grady_thresh=(20, 100),
+                 mag_thresh=(20, 100),
+                 dir_thresh = (0, np.pi / 8)):
+        self.ksize = ksize
+        self.gradx_thresh = gradx_thresh
+        self.grady_thresh = grady_thresh
+        self.mag_thresh = mag_thresh
+        self.dir_thresh = dir_thresh
+
+    def _abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh_min=0, thresh_max=255):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # TODO only sobel x required?
+        # Apply x or y gradient with the OpenCV Sobel() function
+        # and take the absolute value
+        if orient == 'x':
+            abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+        if orient == 'y':
+            abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+        # Rescale back to 8 bit integer
+        scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+        binary_output = np.zeros_like(scaled_sobel)  # TODO why create a copy?
+        binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+        return binary_output
+
+    # Define a function to return the magnitude of the gradient
+    # for a given sobel kernel size and threshold values
+    def _mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # Take both Sobel x and y gradients
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        # Calculate the gradient magnitude
+        gradmag = np.sqrt(sobelx ** 2 + sobely ** 2)
+        # Rescale to 8 bit
+        scale_factor = np.max(gradmag) / 255
+        gradmag = (gradmag / scale_factor).astype(np.uint8)
+        # Create a binary image of ones where threshold is met, zeros otherwise
+        binary_output = np.zeros_like(gradmag)
+        binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+        # Return the binary image
+        return binary_output
+
+    # Define a function to threshold an image for a given range and Sobel kernel
+    def _dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
+        # Grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # Calculate the x and y gradients
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        # Take the absolute value of the gradient direction,
+        # apply a threshold, and create a binary image result
+        absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+        binary_output = np.zeros_like(absgraddir)
+        binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+        # Return the binary image
+        return binary_output
+
+    def isolate_lanes(self, img):
+        gradx = self._abs_sobel_thresh(img, orient='x', sobel_kernel=self.ksize, thresh_min=self.gradx_thresh[0],
+                                               thresh_max=self.gradx_thresh[1])
+        grady = self._abs_sobel_thresh(img, orient='y', sobel_kernel=self.ksize, thresh_min=self.grady_thresh[0],
+                                               thresh_max=self.grady_thresh[1])
+        mag_binary = self._mag_thresh(img, sobel_kernel=self.ksize, mag_thresh=self.mag_thresh)
+        dir_binary = self._dir_threshold(img, sobel_kernel=self.ksize, thresh=self.dir_thresh)
+
+        lanes = np.zeros_like(dir_binary)
+        lanes[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+        return lanes
