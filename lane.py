8ca702734d224f5bff927618e09d8f4900443afb
Marc Neumann <marc.neumann@bmw.de>
2018-01-10 07:08:39 +0100

    moved functions to FittedLane

---
+++ lane.py
@@ -0,0 +1,124 @@
+import numpy as np
+import cv2
+
+
+class FittedLane(object):
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/640 # meters per pixel in x dimension
+
+    def __init__(self, left_fit, right_fit, left_fit_cr, right_fit_cr, out_img):
+        self.left_fit = left_fit
+        self.right_fit = right_fit
+        self.left_fit_cr = left_fit_cr
+        self.right_fit_cr = right_fit_cr
+        self.out_img = out_img
+
+    def deviation_from_lane_center_meters(self):
+        y = self.out_img.shape[1] - 1
+        left_x = self.left_fit[0] * y ** 2 + self.left_fit[1] * y + self.left_fit[2]
+        right_x = self.right_fit[0] * y ** 2 + self.right_fit[1] * y + self.right_fit[2]
+        camera_x = (right_x - left_x) / 2 + left_x
+        center_x = self.out_img.shape[0] / 2
+        deviation_x = camera_x - center_x
+        deviation_meters = deviation_x * FittedLane.xm_per_pix
+        return deviation_meters
+
+    def lane_radius_meters(self):
+        y_eval = self.out_img.shape[1] - 1
+        left_fit_cr = self.left_fit_cr
+        right_fit_cr = self.right_fit_cr
+        left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * FittedLane.ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+            2 * left_fit_cr[0])
+        right_curverad = (
+                         (1 + (2 * right_fit_cr[0] * y_eval * FittedLane.ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+            2 * right_fit_cr[0])
+        return left_curverad, right_curverad
+
+    @staticmethod
+    def fit(img):
+        """
+        :param img: a binary image containing mostly left and right lane markings
+        :return: a FittedLane
+        """
+        # Create an output image to draw on and  visualize the result
+        out_img = np.dstack((img, img, img)) * 255
+        # Find the peak of the left and right halves of the histogram
+        # These will be the starting point for the left and right lines
+        histogram = np.sum(img[img.shape[0] // 2:, :], axis=0)
+        midpoint = np.int(histogram.shape[0] / 2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Choose the number of sliding windows
+        nwindows = 9
+        # Set height of windows
+        window_height = np.int(img.shape[0] / nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = img.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        # Set the width of the windows +/- margin
+        margin = 100
+        # Set minimum number of pixels found to recenter window
+        minpix = 50
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = img.shape[0] - (window + 1) * window_height
+            win_y_high = img.shape[0] - window * window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+                          (0, 255, 0), 2)
+            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
+                          (0, 255, 0), 2)
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                              (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                               (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Colorize the left and right lane pixels
+        # TODO
+        # out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        # out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        left_fit_cr = np.polyfit(lefty * FittedLane.ym_per_pix, leftx * FittedLane.xm_per_pix, 2)
+        right_fit_cr = np.polyfit(righty * FittedLane.ym_per_pix, rightx * FittedLane.xm_per_pix, 2)
+
+        return FittedLane(left_fit, right_fit, left_fit_cr, right_fit_cr, out_img)
+
+
+
